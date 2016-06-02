3581d4dcea2eb73b37877ac392b6f8202316b3e3
Dmitry Smirnov <dsmirnov@wildapricot.com>
2016-06-02 16:16:34 +0300

    Add fake implementation for ICurrencyProvider

---
+++ ZelCurrency.Core/FakeCurrencyProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace ZelCurrency.Core
+{
+    using System.Collections.Generic;
+
+    public class FakeCurrencyProvider : ICurrencyProvider
+    {
+        public IEnumerable<ICurrencyInfo> CurrencyInfos()
+        {
+            var result = new List<ICurrencyInfo>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                result.Add(new FakeCurrencyInfo("bank" + i, "http://sbrf.ru", "some address " + i));
+            }
+
+            return result;
+        }
+    }
+}
